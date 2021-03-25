535f634a01a355974d9b203df63c366ea6eea688
justinpayan <jpayan@cs.umass.edu>
2021-03-25 17:48:29 -0400

    can do algo for n agents in a path

--- efx_subsets.py
+++ efx_subsets.py
@@ -1,6 +1,8 @@
 import numpy as np
+from numpy.random import default_rng
+import sys
+
 from itertools import permutations, chain, combinations, product
-from copy import deepcopy
 
 
 def strong_envy_in_triplet(alloc, val_fns):
@@ -42,7 +44,7 @@ def efx_among_triplet(val_fns):
     # Fix L then fix R. Don't return to a previously seen state.
     strong_envy = np.inf
     envy = np.inf
-    counter = -1*np.inf
+    counter = -1 * np.inf
     while True:
         if counter > 0:
             print("counter: ", counter)
@@ -60,7 +62,7 @@ def efx_among_triplet(val_fns):
             L_goods = set(alloc[0]) | set(alloc[1])
             # print(L_goods)
             for a0 in powerset(L_goods):
-                alloc_prime = (tuple(sorted(list(a0))), tuple(sorted(list(L_goods-set(a0)))), alloc[2])
+                alloc_prime = (tuple(sorted(list(a0))), tuple(sorted(list(L_goods - set(a0)))), alloc[2])
                 # print(alloc_prime)
                 # print()
 
@@ -82,7 +84,7 @@ def efx_among_triplet(val_fns):
         if not R_efx:
             R_goods = set(alloc[1]) | set(alloc[2])
             for a1 in powerset(R_goods):
-                alloc_prime = (alloc[0], tuple(sorted(list(a1))), tuple(sorted(list(R_goods-set(a1)))))
+                alloc_prime = (alloc[0], tuple(sorted(list(a1))), tuple(sorted(list(R_goods - set(a1)))))
 
                 if alloc_prime in already_visited and is_efx(alloc_prime[1], alloc_prime[2], 1, 2, val_fns):
                     print("in R")
@@ -119,6 +121,61 @@ def efx_among_triplet(val_fns):
         counter += 1
 
 
+"""
+Given this allocation and these valuations, rebalance the goods that e[0] and e[1] are currently assigned so 
+that they are EFX with each other.
+"""
+
+
+def fix_edge(alloc, val_fns, e):
+    good_set = set(alloc[e[0]]) | set(alloc[e[1]])
+    for a0 in powerset(good_set):
+        g0 = list(a0)
+        g1 = list(good_set - set(a0))
+
+        if is_efx(g0, g1, e[0], e[1], val_fns):
+            alloc[e[0]] = g0
+            alloc[e[1]] = g1
+            return alloc
+    if not is_efx(alloc[e[0]], alloc[e[1]], e[0], e[1], val_fns):
+        return None
+
+
+"""
+val_fns: nxm numpy array, the ij element is the value of good j for agent i
+edge_list: list of tuples (i, i'), which indicate the edges along which we must be EFX, and they also
+            indicate the order for rebalancing goods under the iterative algorithm
+"""
+
+
+def efx_on_graph(val_fns, edge_list):
+    n, m = val_fns.shape
+
+    alloc = [list(range(m))] + [list() for _ in range(n - 1)]
+
+    # TODO: is this necessary?
+    # already_visited = {alloc}
+
+    # Fix edges in edge_list in forward then reverse order.
+    g_efx = False
+    ctr = 0
+    while not g_efx:
+        # print(ctr)
+        ctr += 1
+        g_efx = True
+        for e in edge_list:
+            if not is_efx(alloc[e[0]], alloc[e[1]], e[0], e[1], val_fns):
+                g_efx = False
+                alloc = fix_edge(alloc, val_fns, e)
+        # Go backwards now. We just finished the last edge, so skip it.
+        for e in edge_list[::-1][1:]:
+            if not is_efx(alloc[e[0]], alloc[e[1]], e[0], e[1], val_fns):
+                g_efx = False
+                alloc = fix_edge(alloc, val_fns, e)
+
+    return alloc
+
+
 def is_efx(alloc1, alloc2, a1, a2, val_fns):
     for i in alloc1:
         if np.sum(val_fns[a2, alloc2]) < np.sum(val_fns[a2, alloc1]) - val_fns[a2, i]:
@@ -136,7 +193,7 @@ def get_valuations(alloc1, alloc2, val_fns):
 def powerset(iterable):
     "powerset([1,2,3]) --> () (1,) (2,) (3,) (1,2) (1,3) (2,3) (1,2,3)"
     s = list(iterable)
-    return chain.from_iterable(combinations(s, r) for r in range(len(s)+1))
+    return chain.from_iterable(combinations(s, r) for r in range(len(s) + 1))
 
 
 # Is it the case that in any EFX allocation on 2 agents, for every subset of a1's goods there is a subset of a2's goods
@@ -144,7 +201,7 @@ def powerset(iterable):
 def for_every_subset_is_there_a_subset():
     np.random.seed(2)
     num_goods = 7
-    valuations = np.random.randint(low=1, high=10, size=(2,num_goods))
+    valuations = np.random.randint(low=1, high=10, size=(2, num_goods))
 
     print(valuations)
 
@@ -198,18 +255,37 @@ def exhaustive_sim_triplet(algo):
 
 if __name__ == "__main__":
     # exhaustive_sim_triplet(efx_among_triplet)
+    n = 6
+    edges = [(i, i + 1) for i in range(n - 1)]
+
+    m = 15
+
+    # valuations = np.array([[2, 2, 6, 6, 8, 7, 7, 3, 4, 4],
+    #                        [4, 5, 1, 3, 6, 4, 2, 8, 5, 7],
+    #                        [4, 1, 5, 9, 2, 6, 8, 8, 8, 3],
+    #                        [7, 3, 2, 6, 9, 4, 6, 6, 6, 5]])
+    # print(efx_on_graph(valuations, edges))
+
     for s in range(100000):
-        if s % 5000 == 0:
-            print(s)
         np.random.seed(s)
 
-        num_goods = 6
-        valuations = np.random.randint(low=1, high=10, size=(3, num_goods))
+        rng = default_rng()
+        vals = rng.standard_normal(10)
+        more_vals = rng.standard_normal(10)
+        valuations = rng.exponential(scale=10, size=(n, m))
+        # valuations = np.random.randint(low=1, high=100, size=(n, m))
+        # valuations *= 1/np.sum(valuations, axis=1)
         # valuations = np.array([[4, 2, 5, 7, 1], [8, 7, 4, 1, 5], [8, 3, 5, 1, 6]])
 
-        # print(valuations)
-        if not efx_among_triplet(valuations):
+        if not efx_on_graph(valuations, edges):
             print(valuations)
-        # print(efx_among_triplet(valuations))
+            sys.exit(1)
 
-
+        if s % 100 == 0:
+            print(s)
+            print(valuations)
+            print(efx_on_graph(valuations, edges))
+        # print(valuations)
+        # if not efx_among_triplet(valuations):
+        #     print(valuations)
+    # print(efx_among_triplet(valuations))
